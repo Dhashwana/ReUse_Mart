917439677c25e9086f3188d68601f2ae4769c95a
Dhashwana <25cs048@rmkcet.ac.in>
2026-02-19 20:19:36 +0530

    Final

--- HomeJS.js
+++ HomeJS.js
@@ -1,11 +1,13 @@
 const menuBtn = document.getElementById("menuBtn");
 const dropdown = document.getElementById("dropdownMenu");
 
-menuBtn.addEventListener("click", (event) => {
-    event.stopPropagation();
-    dropdown.classList.toggle("show");
-});
+if (menuBtn && dropdown) {
+    menuBtn.addEventListener("click", (event) => {
+        event.stopPropagation();
+        dropdown.classList.toggle("show");
+    });
 
-window.addEventListener("click", () => {
-    dropdown.classList.remove("show");
-});
+    window.addEventListener("click", () => {
+        dropdown.classList.remove("show");
+    });
+}
--- ReuseJS.js
+++ ReuseJS.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("logoutBtn").addEventListener("click", () => {
-    localStorage.removeItem("loggedInUser");
-    window.location.href = "index.html";
-});
-
     const modal = document.getElementById("reuseModal");
     const openBtn = document.querySelector(".add-btn");
     const closeBtn = document.getElementById("closeReuseModal");
